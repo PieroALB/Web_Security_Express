bd2ce5a83db65ff1d2aad7906eb0c2e2b5fa9320
Tobi Hatake <tobihatakerin@gmail.com>
2021-11-19 00:09:10 -0500

    coding project

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const express = require('express'),
 
 app.set('view engine','ejs');
 app.set('port',3000);
-app.set('views',path.join(__dirname,'views'));
 app.set('layout',path.join(__dirname,'/views/layouts/layout'));
+app.set('views',path.join(__dirname,'views'));
 
 
 app.use('/public',express.static(path.join(__dirname,'public')));
