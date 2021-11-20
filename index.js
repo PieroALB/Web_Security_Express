5c93670238b3d92c1960fbe129f5490e9f6a2504
Tobi Hatake <tobihatakerin@gmail.com>
2021-11-20 00:06:02 -0500

    coding project

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express'),
   path = require('path'),
   morgan = require('morgan'),
-  expressLayouts = require('express-ejs-layouts'),
   routes = require('./routes/route'),
   app = express();
 
 app.set('view engine','ejs');
 app.set('port',3000);
-app.set('layout',path.join(__dirname,'/views/layouts/layout'));
 app.set('views',path.join(__dirname,'views'));
 
 
@@ -15,7 +13,6 @@ app.use('/public',express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:false}))
 app.use(morgan('dev'));
 app.use(routes);
-app.use(expressLayouts);
 
 app.listen(app.get('port'),()=>{
   console.log(`Server on port: http://localhost:3000`);
